afbbc699c224a92227dc80cb663012aafea4f5e9
sakshi verma <sakshiv1357@gmail.com>
2020-07-30 14:18:03 +0530

    /* this is average Filter method */

--- app/src/main/java/com/vehicletracking/filter/lowpassfilter.java
+++ app/src/main/java/com/vehicletracking/filter/lowpassfilter.java
@@ -1,4 +1,19 @@
 package com.vehicletracking.filter;
 
-public class lowpassfilter {
+
+public class lowpassfilter extends AverageFilter {
+    private static final String tag = lowpassfilter.class.getSimpleName();
+
+    // Gravity and linear accelerations components for the
+    // Wikipedia low-pass filter
+    private float[] output;
+
+    public lowpassfilter() {
+        this(DEFAULT_TIME_CONSTANT);
+    }
+
+    public lowpassfilter(float timeConstant) {
+        this.timeConstant = timeConstant;
+        reset();
+    }
 }
---
+++ app/src/main/java/com/vehicletracking/filter/meanfilter.java
@@ -0,0 +1,4 @@
+package com.vehicletracking.filter;
+
+public class meanfilter {
+}
